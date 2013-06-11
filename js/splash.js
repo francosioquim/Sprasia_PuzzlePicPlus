7b5364306c53b4cc10e420b58935323b65db2941
Franco Sioquim <francosioquim@gmail.com>
2013-06-11 10:56:27 +0800

    Update

--- js/splash.js
+++ js/splash.js
@@ -11,5 +11,6 @@ function deviceReady() {
 }
     
 function removeSplashScreen() {
-    document.addEventListener("deviceready", deviceReady, true);    
+	deviceReady();
+    //document.addEventListener("deviceready", deviceReady, true);    
 }
